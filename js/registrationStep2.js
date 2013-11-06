2cda09eab4e8529ed1ad71c154e483c9f598ff6c
EnergyButterfly <annetka.lviv@gmail.com>
2013-11-06 18:22:02 +0200

    Fixed setting width in magicsuggest js.

--- js/registrationStep2.js
+++ js/registrationStep2.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    var jsonData = [];
+    var cities = 'New York,Los Angeles,Chicago,Houston,Paris,Marseille,Toulouse,Lyon,Bordeaux,Philadelphia,Phoenix,San Antonio,San Diego,Dallas,San Jose,Jacksonville'.split(',');
+    for (var i = 0; i < cities.length; i++) jsonData.push({ id: i, name: cities[i], status: i % 2 ? 'Already Visited' : 'Planned for visit', coolness: Math.floor(Math.random() * 10) + 1 });
+    var ms4 = $('#ms4').magicSuggest({
+        data: [{ id: 1, label: 'one' }, { id: 2, label: 'two' }, { id: 3, label: 'three' }],
+        displayField: 'label',
+        value: [1, 3]
+    });
+
+
     $('#btnBack').on('click', function () {
         document.location = "registration.html";
     });
