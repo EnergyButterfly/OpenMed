5942500a408600d9e46cb3ae58fa91c1fdf57a82
Vadim Stovbunsky <vaderxv@gmail.com>
2013-11-26 17:35:34 +0200

    Dropdowns for the main page + sign in section in header + error messages on the main page (sign in and secret code sections);

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
 $(document).ready(function () {
-    $('#frmRegistration').validate({
-        rules: {
-            inputVerifyPassword: {
-                required: true,
-                minWidth: 2
-            },
-            inputEmail: {
-                required: false
-            }
-        },
-        highlight: function (element) {
-            $(element).closest('.input-group').removeClass('success').addClass('has-error');
-        },
-        success: function (element) {
-            element
-            .text('OK!').addClass('valid')
-            .closest('.input-group').removeClass('error').addClass('has-success');
-        }
+    // $('#frmRegistration').validate({
+    //     rules: {
+    //         inputVerifyPassword: {
+    //             required: true,
+    //             minWidth: 2
+    //         },
+    //         inputEmail: {
+    //             required: false
+    //         }
+    //     },
+    //     highlight: function (element) {
+    //         $(element).closest('.input-group').removeClass('success').addClass('has-error');
+    //     },
+    //     success: function (element) {
+    //         element
+    //         .text('OK!').addClass('valid')
+    //         .closest('.input-group').removeClass('error').addClass('has-success');
+    //     }
+    // });
+
+    // DEBUG -> Only to test Sign In button interactions
+    $("#btn-signin").click(function () {
+        $("#btn-signin").hide();
+        $("#header-signin-error").show();
+        $("#section-signin").show();
+    });
+
+    // DEBUG -> Only to test Secret Code error functionality
+    $("#btn-go").click(function () {
+        $("#secret-code-error").show();
     });
 });
 
